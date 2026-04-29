eec6fdacbbf52681ee747d59e06c99dbf7ce61fc
zaakir10 <zaakirhasan10@gmail.com>
2026-04-29 14:35:12 +0300

    fix: zod validation issues

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -27,7 +27,7 @@ export const validate = (schema) => (req, res, next) => {
     next();
   } catch (error) {
     if (error instanceof z.ZodError) {
-      const messages = error.errors.map((err) => err.message);
+      const messages = error.issues.map((err) => err.message);
       return res.status(400).json({ success: false, errors: messages });
     }
     next(error);
